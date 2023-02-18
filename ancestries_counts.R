e221d0e5a8585193b5ba5ddf3eeccbaff68de185
Carlos Passos <crlshnrqpassos@gmail.com>
2023-02-18 10:06:21 -0300

    Update ancestries_counts.R

--- ancestries_counts.R
+++ ancestries_counts.R
@@ -13,6 +13,9 @@ library(data.table)
 lai_inferences <-
   read_delim("/home/chpassos/Analises/REDS-III/ancestries/lai/chr11_output/query_results.msp",
              skip = 1)
+# Saving info
+info <- lai_inferences |>
+  select(c(1:6))
 
 # Removing the first six columns
 lai <- lai_inferences |>
@@ -76,6 +79,45 @@ sum_eur <- map2(eur_dot0, eur_dot1,
 # Summing NAM counts
 sum_nam <- map2(nam_dot0, nam_dot1, 
                 \(nam_dot0, nam_dot1) nam_dot0 + nam_dot1)
+
+# Putting info columns back in the data
+## African Ancestry
+tbl_afr <- as_tibble(sum_afr)
+afr_count <- info |>
+  cbind(tbl_afr) |>
+  as_tibble()
+
+## European Ancestry
+tbl_eur <- as_tibble(sum_eur)
+eur_count <- info |>
+  cbind(tbl_eur) |>
+  as_tibble()
+
+## Native American Ancestry
+tbl_nam <- as_tibble(sum_nam)
+nam_count <- info |>
+  cbind(tbl_nam) |>
+  as_tibble()
+
+
+# What happens now is that we have the ancestries count for EACH ANCESTRY
+# But only in the INTERVAL estimated by Gnomix
+# I need to get the ancestry count per position
+## It was best to do first in intervals, because otherwise the data
+## would have been very big to work with.
+## Now I need to do a conditional join of each ancestry count tbl
+## to a file that has the positions in the vcf data :)
+
+## Get positions in VCF file used in Gnomix
+# bcftools query -f '%POS\n' REDS_chr11_gnomix.vcf.gz > chr11_pos.txt
+
+# Read this file
+pos <- 
+  read.table("/home/chpassos/Analises/REDS-III/ancestries/lai/chr11_output/chr11_pos.txt")
+head(pos)
+
+# Conditional join
+
 
 
 
