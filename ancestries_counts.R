53d28a40e21a1200ef7a19a6f7ea3a65d3eb21b2
Carlos Passos <crlshnrqpassos@gmail.com>
2023-02-20 16:25:04 -0300

    Update ancestries_counts.R

--- ancestries_counts.R
+++ ancestries_counts.R
@@ -13,6 +13,7 @@ library(sqldf) # For conditional join
 lai_inferences <-
   read_delim("/home/chpassos/Analises/REDS-III/ancestries/lai/chr11_output/query_results.msp",
              skip = 1)
+
 # Saving info
 info <- lai_inferences |>
   select(c(1:6))
@@ -37,7 +38,6 @@ df_dot0 <- lai |>
 id_order <- colnames(df_dot0) # Separating column names, to order the DF
 df_dot0 <- df_dot0 |>
   select(order(id_order)) # Rearranging the column order of dot0
-
 # Renaming columns
 df_dot1 <- lai |>
   select(any_of(dot1$names.lai.)) |>
@@ -120,38 +120,25 @@ nam_count <- info |>
 # Read this file
 pos <- 
   read.table("/home/chpassos/Analises/REDS-III/ancestries/lai/chr11_output/chr11_pos.txt") |>
-  mutate(pos = "11")
-
-# Gnomix intervals were estimated with ~500 SNPs per interval
-# I will try to keep 20% of that dataset
-## 20% of the positions present in the vcf
-pos_to_keep <- round(nrow(pos) * 0.1)
-# Sampling 20% of the positions, with no replacement
-new_pos <- pos |>
-  sample_n(size = pos_to_keep,
-           replace = FALSE)
+  mutate(chr = "11") |>
+  rename("position" = "V1")
 
 # Files of interest
-head(new_pos)
+head(pos)
 head(afr_count)
 head(eur_count)
 head(nam_count)
 
 # Conditional join
-## For the join to work, I do need to make the count df tidy..
-## The conditional join could take a while
-### African Ancestry conditional join
-tidy_afr <- afr_count |>
-  mutate(row_id = row_number()) |>
-  pivot_longer(-c(`#chm`, "spos", "epos", "sgpos", "egpos", `n snps`, "row_id"),
-               names_to = "sample_id",
-               values_to = "count") |>
-  rename("chromosome" = `#chm`,
-         "n_snps" = `n snps`)
+# I need to divide my dataset - It has so many columns!
+afr_count_d1 <- afr_count |> select(1:1376)
+afr_part1_joined <- sqldf('SELECT * FROM afr_count_d1 LEFT JOIN pos ON position BETWEEN spos and epos')
 
-joined_afr <- sqldf('SELECT chromosome, spos, epos, sample_id, count 
-      FROM tidy_afr 
-      LEFT JOIN new_pos ON V1 BETWEEN spos and epos')
+afr_count_d2 <- afr_count |> select(c(1:6, 1377:ncol(afr_count)))
+afr_part2_joined <- sqldf('SELECT * FROM afr_count_d2 LEFT JOIN pos ON position BETWEEN spos and epos')
+
+  
+
 
 ### European Ancestry
 tidy_eur <- eur_count |>
